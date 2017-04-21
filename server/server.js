b78b19dd120ade2b7f0dd81f3790bdb90ebfa07f
Abdan Syakuron <abdan.s@students.amikom.ac.id>
2017-04-21 13:01:37 +0700

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-var {mongoose} = require('./db/mongoose');
-var {Todo} = require('./models/todo');
-var {User} = require('./models/user');
+const {ObjectID} = require('mongodb');
+const {mongoose} = require('./db/mongoose');
+const {Todo} = require('./models/todo');
+const {User} = require('./models/user');
 
 var app = express();
 
@@ -27,6 +28,23 @@ app.get('/todos', (req, res) => {
     },(e) => {
         res.status(400).send(e);
     });
+});
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if (!ObjectID.isValid(id))
+        return res.status(400).send();
+
+    Todo.findById(id).then((todo) => {
+        if (!todo)
+            return res.status(404).send();
+
+        res.status(200).send({todo});
+    }, (e) => {
+        res.status(400).send();
+    });
+
 });
 
 app.listen(3000, () => {
