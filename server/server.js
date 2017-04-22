0e50e9d6576702e3c55588e2db1648c884bfe9ef
Abdan Syakuron <abdan.s@students.amikom.ac.id>
2017-04-22 14:42:20 +0700

    Add POST /users route and token method

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+require('./config/config');
+
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -82,6 +84,16 @@ app.patch('/todos/:id', (req, res) => {
 
         res.send({todo});
     }).catch((e) => res.status(400).send());
+});
+
+app.post('/users', (req, res) => {
+    var user = new User(_.pick(req.body, ['email', 'password']));
+
+    user.save().then(() => {
+        return user.generateAuthToken();
+    }).then((token) => {
+        res.header('x-auth', token).send(user);
+    }).catch((e) => res.status(400).send(e));
 });
 
 app.listen(port, () => {
