b15650ed6a11421bbce35b28c5f67ecba20332b0
syakuronabdan <abdan.s@students.amikom.ac.id>
2017-05-20 06:47:45 +0700

    Update config to use json file

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI)
+    .catch((e) => console.log(e));
 
 module.exports = {mongoose};
