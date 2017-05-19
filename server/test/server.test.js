e5f8c5cb6bb34550832c7d63c39f1167af4cc47d
syakuronabdan <abdan.s@students.amikom.ac.id>
2017-05-19 12:49:03 +0700

    Add tests for DELETE /users/me/token

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -193,7 +193,7 @@ describe('GET /users/me', () => {
 });
 
 describe('POST /users', () => {
-   it('should create a user', (done) => {
+    it('should create a user', (done) => {
        var email = 'test3@test.com';
        var password = '123mb!';
 
@@ -275,4 +275,21 @@ describe('POST /users/login', () => {
                }).catch((e) => done(e));
            });
    });
+});
+
+describe('DELETE /users/me/token', () => {
+    it('should remove auth token on logout', (done) => {
+        request(app)
+            .delete('/users/me/token')
+            .set('x-auth', users[0].tokens[0].token)
+            .expect(200)
+            .end((err, res) => {
+                if (err) return done(err);
+
+                User.findById(users[0]._id).then((user) => {
+                   expect(user.tokens.length).toBe(0);
+                   done();
+                }).catch((e) => done(e));
+            });
+    });
 });
